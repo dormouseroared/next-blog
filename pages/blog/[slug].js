e84260340f8b2a364a3edf11a9a2a0467c087cd8
dormouseroared <dormouseroared@protonmail.com>
2022-10-02 18:17:30 +0100

    omit author for slug, change slug headings, add excerpt

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,41 +1,50 @@
 import fs from "fs"
 import path from "path"
-import {marked} from "marked"
+import { marked } from "marked"
 import matter from "gray-matter"
 import Link from "next/link"
 import Layout from "@/components/Layout"
 import CategoryLabel from "@/components/CategoryLabel"
 
-export default function SlugPage({ frontMatter: {title, category, date, cover_image, author, author_image}, content, slug }) {
+export default function SlugPage({ frontMatter: { title, category, date, cover_image, author, author_image, excerpt }, content, slug }) {
 
     return (
         <Layout title={title} component="/blog/[slug] SlugPage">
-            <Link href="/blog">Go Back</Link>
+            {/* <Link href="/blog">Go Back</Link> */}
 
             <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
-                <div className="flex justify-between items-center mt-4 mb-4">
-                    <h1 className="text-2xl sm:text-3xl">
+
+                <div className="flex justify-between items-center mt-4 bg-gray-500 text-gray-100 p-5 rounded-lg">
+
+                    <h1 className="text-2xl sm:text-3xl lg:text-4xl">
                         {title}
                     </h1>
                     <CategoryLabel>
                         {category}
                     </CategoryLabel>
+
                 </div>
 
-                <img src={cover_image} alt="" className="w-full rounded"/>
-
-                <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
-                    <div className="flex items-center">
-                        <img src={author_image} alt="" className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"/>
-                        <h4>{author}</h4>
-                    </div>
-                    <div className="mr-4">
+                <div className="flex justify-between items-center bg-gray-500 text-gray-100 p-5 rounded-lg mt-1 mb-6 text-xs lg:text-2xl">
+                    <h3>{excerpt}</h3>
+                    <div className="">
                         {date}
                     </div>
                 </div>
 
+
+
+                {/* <img src={cover_image} alt="" className="w-full rounded"/> */}
+
+                {/* <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+                    <div className="flex items-center">
+                        <img src={author_image} alt="" className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"/>
+                        <h4>{author}</h4>
+                    </div>
+                </div> */}
+
                 <div className="markdown-content blog-text prose-lg lg:prose-xl mt-2">
-                    <div dangerouslySetInnerHTML={{__html: marked(content)}}></div>
+                    <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
                 </div>
             </div>
         </Layout>
