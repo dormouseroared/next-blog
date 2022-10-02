4f46e6d6380ecb65e9c41b8aa15b0225e8ca3b2f
dormouseroared <dormouseroared@protonmail.com>
2022-10-02 20:13:57 +0100

    add background to post card

--- components/Post.js
+++ components/Post.js
@@ -5,7 +5,7 @@ import CategoryLabel from "@/components/CategoryLabel"
 export default function Post({post}) {
   return (
 
-    <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+    <div className="w-full p-6 bg-gray-200 rounded-lg shadow-lg mt-6">
 
         <Image src={post.frontMatter.cover_image} alt="" height={420} width={600} className="mb-4 rounded" />
 
