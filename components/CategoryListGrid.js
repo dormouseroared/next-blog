8ceee90ab858f03d40fa80078d0a0becfa5754b6
dormouseroared <dormouseroared@protonmail.com>
2022-10-05 09:57:56 +0100

    CategoryListGrid: remove top rounding & 6 grid columns on xl

--- components/CategoryListGrid.js
+++ components/CategoryListGrid.js
@@ -5,11 +5,11 @@ export default function CategoryList({ categories }) {
   return (
     <div className='w-full mt-6'>
 
-      <h3 className='text-3xl bg-slate-700 text-slate-100 p-3 rounded-t-2xl'>
+      <h3 className='text-3xl bg-slate-700 text-slate-100 p-3'>
         Categories available
       </h3>
 
-      <div className='mt-6 grid gap-2 grid-cols-3 lg:grid-cols-5'>
+      <div className='mt-6 grid gap-2 grid-cols-3 lg:grid-cols-5 xl:grid-cols-6'>
         {categories.map((category, index) => (
           <Link key={index} href={`/blog/category/${category.toLowerCase()}`}>
             <div className='cursor-pointer sm:text-xl rounded-lg'>
