69dfa7d6c2f6920d7764bb75e07a908378323ddd
dormouseroared <dormouseroared@protonmail.com>
2022-10-06 00:53:57 +0100

    daisyUI JSX copied to HeroWithOverlayImage to amend as component with props. Button in progress. New post trying daisyUI. Could not get it working. OK in html. Used About for testing.

---
+++ components/HeroWithOverlayImage.js
@@ -0,0 +1,19 @@
+
+export default function HeroWithOverlayImage({title, image, button, children}) {
+    return (
+        <>
+            <div className="hero min-h-screen" style={{ backgroundImage: `url(${image})` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+                        <p className="mb-5">
+                            {children}
+                        </p>
+                        <button className="btn btn-primary"><a href="#about">{button}</a></button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ markdown-posts/daisyui-kbd.md
@@ -0,0 +1,27 @@
+---
+title: 'daisyUI kbd'
+date: 'October 5, 2022'
+excerpt: 'Try out some daisyUI with the kbd component'
+cover_image: '/images/posts/daisyUI-kbd.png'
+category: 'NextJS'
+---
+## kbd
+
+In markdown, a key combination can be shown using html.
+
+<kbd className="kbd">ctrl</kbd>
++
+<kbd className="kbd">shift</kbd>
++
+<kbd className="kbd">del</kbd>
+
+
+
+
+
+
+
+
+
+
+
