90879920afae836b4d8abf7a415d5a273249b099
dormouseroared <dormouseroared@protonmail.com>
2022-10-11 09:16:15 +0100

    console.log for AnotherButton after error in vercel Uncaught TypeError: i is not a function

--- components/AnotherButton.js
+++ components/AnotherButton.js
@@ -8,8 +8,9 @@ export default function AnotherButton() {
     const {widthParam, setWidth} = useState(55)
 
     function handleClick() {
-        console.log(window.innerWidth)
+        console.log("before setWidth", window.innerWidth, window.innerHeight)
         setWidth(window.innerWidth)
+        console.log("after setWidth", window.innerWidth, window.innerHeight)
     }
     
     return (
