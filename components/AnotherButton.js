ce8494b31d859412f3ac6b31a74e0b98185fe212
dormouseroared <dormouseroared@protonmail.com>
2022-10-11 10:56:40 +0100

    AnotherButton now shows window width and height

--- components/AnotherButton.js
+++ components/AnotherButton.js
@@ -1,19 +1,23 @@
 import { useState } from "react"
 
-const description = "WIDTH GOES HERE: "
 
 
 export default function AnotherButton() {
 
     const [widthParam, setWidth] = useState(55)
+    const [heightParam, setHeight] = useState(55)
+
     console.log("[AnotherButton] check array used to destructure setWidth function:", typeof setWidth)
 
     function handleClick() {
         console.log("[AnotherButton] width x height:", window.innerWidth, "x", window.innerHeight)
         setWidth(window.innerWidth)
+        setHeight(window.innerHeight)
     }
     
     return (
-        <button onClick={handleClick}>{description}{widthParam}</button>
+        <button onClick={handleClick} className="my-6 btn btn-secondary">
+            width x height: {widthParam} x {heightParam}
+        </button>
     )
 }
