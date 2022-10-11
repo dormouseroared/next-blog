31096eddb30b4ab336713edf79f535578575969f
dormouseroared <dormouseroared@protonmail.com>
2022-10-11 10:39:26 +0100

    AnotherButton error in code fixed so destructure useState into array now rather than object

--- components/AnotherButton.js
+++ components/AnotherButton.js
@@ -5,12 +5,12 @@ const description = "WIDTH GOES HERE: "
 
 export default function AnotherButton() {
 
-    const {widthParam, setWidth} = useState(55)
+    const [widthParam, setWidth] = useState(55)
+    console.log("[AnotherButton] check array used to destructure setWidth function:", typeof setWidth)
 
     function handleClick() {
-        console.log("before setWidth", window.innerWidth, window.innerHeight)
+        console.log("[AnotherButton] width x height:", window.innerWidth, "x", window.innerHeight)
         setWidth(window.innerWidth)
-        console.log("after setWidth", window.innerWidth, window.innerHeight)
     }
     
     return (
